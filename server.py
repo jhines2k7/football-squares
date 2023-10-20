b5d6844173612b1f01641177239bc3e56fb08ad6
James Hines <jameshines10@gmail.com>
2023-10-20 16:20:33 +0000

    Initial commit

---
+++ server.py
@@ -0,0 +1,64 @@
+import datetime
+import sys
+import logging
+import uuid
+import json
+import argparse
+import os
+import math
+import queue
+import binascii
+
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google.oauth2 import service_account
+from googleapiclient.http import MediaIoBaseDownload
+from gevent import monkey
+monkey.patch_all()
+
+from flask import Flask, jsonify, request
+from flask_socketio import SocketIO, join_room, emit, leave_room
+from flask_cors import CORS
+from web3 import Web3
+from web3.middleware import geth_poa_middleware
+from eth_account import Account
+from dotenv import load_dotenv
+from logging.handlers import RotatingFileHandler
+from datetime import datetime
+
+logging.basicConfig(
+  stream=sys.stderr,
+  level=logging.DEBUG,
+  format='%(levelname)s:%(asctime)s:%(message)s'
+)
+
+logger = logging.getLogger(__name__)
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your-secret-key'
+CORS(app)
+socketio = SocketIO(app, async_mode='gevent', cors_allowed_origins='*')
+
+@socketio.on('heartbeat')
+def handle_heartbeat(data):
+  logger.info('Received heartbeat from client.')
+  logger.info(data)
+  # Emit a response back to the client
+  emit('heartbeat_response', 'pong')
+
+if __name__ == '__main__':
+  from geventwebsocket.handler import WebSocketHandler
+  from gevent.pywsgi import WSGIServer
+  
+  logger.info('Downloading contract ABIs...')
+
+  logger.info('Starting server...')
+
+  http_server = WSGIServer(('0.0.0.0', 443),
+                           app,
+                           keyfile=KEYFILE,
+                           certfile=CERTFILE,
+                           handler_class=WebSocketHandler)
+
+  http_server.serve_forever()  
