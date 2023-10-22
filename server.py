183532f79bd6b6b2fdbc9b1a43f5f5870c349a8e
James Hines <jameshines10@gmail.com>
2023-10-22 15:29:43 +0000

    Players can claim squares

--- server.py
+++ server.py
@@ -40,6 +40,65 @@ app.config['SECRET_KEY'] = 'your-secret-key'
 CORS(app)
 socketio = SocketIO(app, async_mode='gevent', cors_allowed_origins='*')
 
+games = {}
+players = {}
+
+names = ['Dolphins v Patriots', 
+         'Chiefs v Bills', 
+         'Buccaneers v Packers', 
+         'Rams v Seahawks', 
+         'Ravens v Titans', 
+         'Bears v Saints', 
+         'Browns v Steelers',
+         'Colts v Jaguars',
+         'Vikings v Lions',
+         'Jets v Panthers']
+
+def get_new_game(name):
+  return {
+    'id': str(uuid.uuid4()),
+    'name': name,
+    'players': {},
+  }
+
+def get_new_player():
+  return {
+    'id': str(uuid.uuid4()),
+    'game_id': None,
+  }
+
+def send_to_all_except(event, message, game, player_id):
+  for player in game['players'].values():
+    if player['id'] != player_id:
+      emit(event, message, room=player['id'])
+
+@socketio.on('join_game')
+def join_game(data):
+  game = games[data['game_id']]
+
+  if data['player_id'] in game['players']:
+    logger.info(f"Player {data['player_id']} already joined game.")
+    return
+  
+  player = players[data['player_id']]
+  player['game_id'] = game['id']
+
+  game['players'][data['player_id']] = player
+  logger.info(f"Game info: {game}")
+
+  logger.info(f"Player {player['id']} joined game.")
+  emit('game_joined', game, room=player['id'])
+  
+  logger.info(f"Game info: {game}")
+
+  join_room(game['id'])
+
+def generate_games():
+  logger.info('Generating games...')
+  for i in range(0, 9):
+    game = get_new_game(names[i])
+    games[game['id']] = game   
+
 @socketio.on('heartbeat')
 def handle_heartbeat(data):
   logger.info('Received heartbeat from client.')
@@ -47,11 +106,41 @@ def handle_heartbeat(data):
   # Emit a response back to the client
   emit('heartbeat_response', 'pong')
 
+@socketio.on('claim_square')
+def handle_claim_square(data):
+  logger.info(f"Received claim square from client: {data}")
+  game = games[data['game_id']]
+
+  message = {
+    'row': data['row'],
+    'column': data['column'],
+    'claimed_by': data['player_id'],
+  }
+
+  send_to_all_except('square_claimed', message, game, data['player_id'])
+
+@socketio.on('connect')
+def handle_connect():
+  logger.info('Client connected.')
+  player = get_new_player()
+  players[player['id']] = player
+  logger.info(f"Player info: {player}")
+
+  join_room(player['id'])
+
+  games_list = [{"game_id": game["id"], "name": game["name"]} for game in games.values()]
+
+  emit('connected', { 'games_list': games_list, 'player': player }, room=player['id'])
+
+  # join_game(player)
+
 if __name__ == '__main__':
   from geventwebsocket.handler import WebSocketHandler
   from gevent.pywsgi import WSGIServer
   
   logger.info('Starting server...')
+
+  generate_games()
 
   # http_server = WSGIServer(('0.0.0.0', 443),
   #                          app,
